5a120b495669d1a563c0e999ab3990c6b5568591
Daniel Alves <dan_alves96@hotmail.com>
2025-01-14 21:29:20 -0300

    fix css

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,13 @@ import MenuScene from './scenes/MenuScene'
 import allStages from './scenes/stages'
 
 const config: Phaser.Types.Core.GameConfig = {
-  type: Phaser.AUTO,
+  type: Phaser.CANVAS,
   parent: 'game-container',
-  width: 750,
-  height: 500,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    width: 750,
+    height: 500,
+  },
   scene: [BootScene, PreloadScene, MenuScene, ...allStages],
   physics: {
     default: 'arcade',
