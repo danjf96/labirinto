530a6413ab2a56476675d238c41a667e03404ff4
Daniel Alves <dan_alves96@hotmail.com>
2025-01-27 22:25:31 -0300

    changed nomenclatures

---
+++ src/scenes/stages/BaseStage/interfaces.ts
@@ -0,0 +1,11 @@
+import Coin from '../../entities/coin'
+import Maze from '../../entities/maze'
+import Player from '../../entities/player'
+
+interface GameScene extends Phaser.Scene {
+  player: Player
+  coin: Coin
+  maze: Maze
+}
+
+export default GameScene
