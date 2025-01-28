5da504206d2f21fe09aa5cd57b7f099e9d29740d
Daniel Alves <dan_alves96@hotmail.com>
2025-01-27 22:20:04 -0300

    created baseStage

---
+++ src/scenes/stages/BaseStage/index.ts
@@ -0,0 +1,66 @@
+import gameManager from '../../../controls/GameManager'
+import Hud from '../../../controls/Hud'
+import MusicPlayer from '../../../controls/MusicPlayer'
+import Coin from '../../entities/coin'
+import { generatePossiblePositions } from '../../entities/coin/helpers'
+import { CoinPositionProps } from '../../entities/coin/types'
+import Enemy from '../../entities/enemy'
+import Maze from '../../entities/maze'
+import { MazeGridType } from '../../entities/maze/types'
+import Player from '../../entities/player'
+import GameScene from '../../interfaces/GameScene'
+
+class BaseStage extends Phaser.Scene implements GameScene {
+  maze!: Maze
+  player!: Player
+  coin!: Coin
+  hud!: Hud
+  enemyGoblin!: Enemy
+  audio!: MusicPlayer
+
+  constructor({ key }: { key: string }) {
+    super({ key })
+  }
+
+  preload(mazeGrid: MazeGridType) {
+    this.player = new Player(this, { x: 0, y: 0 })
+
+    this.maze = new Maze(this, mazeGrid)
+    this.maze.create()
+
+    this.coin = new Coin(this)
+  }
+
+  create() {
+    this.hud = new Hud(this)
+
+    this.audio = new MusicPlayer(this, 'mainSong')
+    this.audio.setVolume(0.1)
+    this.audio.play(true)
+
+    const width = this.cameras.main.width
+    const height = this.cameras.main.height
+    const background = this.add.sprite(0, 0, 'ground')
+    background.setOrigin(0, 0)
+    background.setDisplaySize(width, height)
+    background.setDepth(-1)
+
+    const coinPositions: CoinPositionProps[] = generatePossiblePositions(
+      this.maze.getGrid(),
+    )
+    this.coin.create(coinPositions[0])
+    this.coin.setPossiblePositions(coinPositions)
+    this.coin.playAnimation()
+
+    this.coin.setColider('GET')
+  }
+
+  update(time: number, delta: number): void {
+    const gameOver = gameManager.getGameOver()
+    if (gameOver) return
+
+    this.player.movePlayer()
+  }
+}
+
+export default BaseStage
--- src/scenes/stages/StageScene1/index.ts
+++ src/scenes/stages/StageScene1/index.ts
@@ -1,29 +1,14 @@
-import gameManager from '../../../controls/GameManager'
-import Hud from '../../../controls/Hud'
-import MusicPlayer from '../../../controls/MusicPlayer'
-import Coin from '../../entities/coin'
 import { generatePossiblePositions } from '../../entities/coin/helpers'
-import { CoinPositionProps } from '../../entities/coin/types'
 import Enemy from '../../entities/enemy'
-import Maze from '../../entities/maze'
 import { MazeGridType } from '../../entities/maze/types'
-import Player from '../../entities/player'
-import GameScene from '../../interfaces/GameScene'
+import BaseStage from '../BaseStage'
 
-class StageScene1 extends Phaser.Scene implements GameScene {
-  maze!: Maze
-  player!: Player
-  coin!: Coin
-  hud!: Hud
-  enemyGoblin!: Enemy
-
+class StageScene1 extends BaseStage {
   constructor() {
     super({ key: 'Stage1' })
   }
 
   preload() {
-    this.player = new Player(this, { x: 0, y: 0 })
-
     const mazeGrid: MazeGridType = [
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -37,35 +22,14 @@ class StageScene1 extends Phaser.Scene implements GameScene {
       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ]
 
+    super.preload(mazeGrid)
+
     const possiblePositions = generatePossiblePositions(mazeGrid)
     this.enemyGoblin = new Enemy(this, possiblePositions[1])
-
-    this.maze = new Maze(this, mazeGrid)
-    this.maze.create()
-
-    this.coin = new Coin(this)
   }
 
   create() {
-    this.hud = new Hud(this)
-
-    const audio = new MusicPlayer(this, 'mainSong')
-    audio.setVolume(0.1)
-    audio.play(true)
-
-    const width = this.cameras.main.width
-    const height = this.cameras.main.height
-    const background = this.add.sprite(0, 0, 'ground')
-    background.setOrigin(0, 0)
-    background.setDisplaySize(width, height)
-    background.setDepth(-1)
-
-    const coinPositions: CoinPositionProps[] = generatePossiblePositions(
-      this.maze.getGrid(),
-    )
-    this.coin.create(coinPositions[0])
-    this.coin.setPossiblePositions(coinPositions)
-    this.coin.playAnimation()
+    super.create()
 
     this.maze.setCollisionsWithTheBlocks([
       this.player.getSprite(),
@@ -73,14 +37,10 @@ class StageScene1 extends Phaser.Scene implements GameScene {
     ])
 
     this.player.loseCoinWhenColide([this.enemyGoblin.getSprite()])
-    this.coin.setColider('GET')
   }
 
   update(time: number, delta: number): void {
-    const gameOver = gameManager.getGameOver()
-    if (gameOver) return
-
-    this.player.movePlayer()
+    super.update(time, delta)
 
     this.enemyGoblin.movePlayer()
   }
